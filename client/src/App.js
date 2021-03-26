2020ce1f69cdb798c4470b9d4924ecc42436beeb
Ayushi-777 <princessayushi777@gmail.com>
2021-03-26 16:17:12 +0530

    navbar complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import "./App.css"
+import {Route} from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 const App = () => {
   return (
     <div>
-      <h1>welcome</h1>
+       <Navbar/>
+
+    <Route path="/">
+    <Home/>
+    </Route>
+
+    <Route path="/about">
+    <About/>
+    </Route>
+
+    <Route path="/contact">
+    <Contact/>
+    </Route>
+
+    <Route path="/login">
+    <Login/>
+    </Route>
+
+    <Route path="/signup">
+    <Signup/>
+    </Route>
+
+   
     </div>
   )
 }
